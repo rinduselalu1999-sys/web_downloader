3a11c538f12faa9ccd94aadcc021944cfff28860
rinduselalu1999-sys <rindu.selalu1999@gmail.com>
2026-05-15 17:38:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-quests
+import requests
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
@@ -48,3 +48,4 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    
